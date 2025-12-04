e933d51e39a075792e981be058f86e98e33e6959
Luisillo377 <lfelipem25@gmail.com>
2025-12-04 20:28:28 -0300

    fix

--- departamento.py
+++ departamento.py
@@ -1,9 +1,10 @@
+# Arreglo recursivo de importacion
 from __future__ import annotations
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
     from empleado import Empleado
-
+# Arreglo recursivo de importacion
 class Departamento:
     def __init__(self, idDepartamento:int, nombre:str, gerente:'Empleado|None' = None, empleados:'list[Empleado]' = []):
         self.idDepartamento = idDepartamento
